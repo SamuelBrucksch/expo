3c6e63783c2845aa9c5ec7d56ed6775f79181277
Vojtech Novak <vonovak@gmail.com>
2025-10-13 18:14:16 +0200

    review

--- apps/native-component-list/src/screens/Image/ImageComparisonScreen.tsx
+++ apps/native-component-list/src/screens/Image/ImageComparisonScreen.tsx
@@ -28,7 +28,9 @@ export function ImageComparisonBody({
   const renderSectionHeader = ({ section }: any) => {
     return (
       <View style={styles.header}>
-        <Text style={styles.title}>{section.title}</Text>
+        <Text style={styles.title} testID={`header-${section.title}`}>
+          {section.title}
+        </Text>
       </View>
     );
   };
